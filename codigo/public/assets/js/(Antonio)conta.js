8ffba1b15b80b0f3e4204bd943377b84671b277e
AntonioCardosoASimoes <antoniocardososimoes@gmail.com>
2026-06-11 00:34:55 -0300

    Criação da página da conta do usuário

---
+++ codigo/public/assets/js/(Antonio)conta.js
@@ -0,0 +1,73 @@
+let dadosUsuario;
+
+fetch("conta.json")
+.then(response => response.json())
+.then(dados => {
+
+    dadosUsuario = dados;
+
+    document.getElementById("nomeUsuario").textContent =
+        dados.usuario.nome;
+
+    document.getElementById("cursoUsuario").textContent =
+        dados.usuario.curso;
+
+    document.getElementById("idadeUsuario").textContent =
+        dados.usuario.idade + " anos";
+
+    document.getElementById("membroDesde").textContent =
+        "Usuário desde " + dados.usuario.membroDesde;
+
+    document.getElementById("descricaoUsuario").textContent =
+        dados.usuario.descricao;
+});
+
+function mostrarConteudo(tipo){
+
+    const area = document.getElementById("areaConteudo");
+
+    let lista = [];
+
+    switch(tipo){
+
+        case "historico":
+            lista = dadosUsuario.historicoConteudos;
+            titulo = "Histórico de Conteúdos";
+            break;
+
+        case "completas":
+            lista = dadosUsuario.atividadesCompletas;
+            titulo = "Atividades Completas";
+            break;
+
+        case "afazer":
+            lista = dadosUsuario.atividadesAFazer;
+            titulo = "Atividades a Fazer";
+            break;
+
+        case "favoritos":
+            lista = dadosUsuario.favoritos;
+            titulo = "Atividades Favoritas";
+            break;
+
+        case "dificuldades":
+            lista = dadosUsuario.dificuldades;
+            titulo = "Dificuldades";
+            break;
+
+        case "psicologos":
+            lista = dadosUsuario.psicologos;
+            titulo = "Psicólogos Recomendados";
+            break;
+    }
+
+    let html = `<h3>${titulo}</h3><ul>`;
+
+    lista.forEach(item => {
+        html += `<li>${item}</li>`;
+    });
+
+    html += "</ul>";
+
+    area.innerHTML = html;
+}
